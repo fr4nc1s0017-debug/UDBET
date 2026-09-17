b62c350aea83c1ff0c1bf294827e47c8af4ed07e
cheche <fr4nc1s0017@gmail.com>
2026-09-17 17:29:26 -0600

    feat (add / agregar) Nueva funcionalidad agregar: Barra de navegacion de pantallas en la pantalla principal

--- app/src/main/java/udb/edu/sv/dsm/udbet/MainActivity.kt
+++ app/src/main/java/udb/edu/sv/dsm/udbet/MainActivity.kt
@@ -5,16 +5,74 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val bottomNavigation =
+            findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+
+            when (item.itemId) {
+
+                R.id.nav_home -> {
+                    Toast.makeText(
+                        this,
+                        "Inicio",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    true
+                }
+
+                R.id.nav_search -> {
+                    Toast.makeText(
+                        this,
+                        "Búsqueda",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    true
+                }
+
+                R.id.nav_sports -> {
+                    Toast.makeText(
+                        this,
+                        "Deportes",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    true
+                }
+
+                R.id.nav_casino -> {
+                    Toast.makeText(
+                        this,
+                        "Casino",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    true
+                }
+
+                R.id.nav_history -> {
+                    Toast.makeText(
+                        this,
+                        "Historial",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
